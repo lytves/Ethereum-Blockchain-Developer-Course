1e3155c1e636ef8e5769f61397d131ff49d170b1
lytves <22356770+lytves@users.noreply.github.com>
2022-05-07 18:10:20 +0200

    WorkingWithVariables

--- contracts/WorkingWithVariables.sol
+++ contracts/WorkingWithVariables.sol
@@ -14,4 +14,24 @@ contract WorkingWithVariables {
     function setMyBool(bool _myBool) public {
         myBool = _myBool;
     }
+
+    uint8 public myUint8;
+
+    function incrementUint() public {
+        myUint8++;
+    }
+
+    function decrementUint() public {
+        myUint8--;
+    }
+
+    address public myAddress;
+
+    function setMyAddress(address _myAddress) public {
+        myAddress = _myAddress;
+    }
+
+    function getBalanceOfMyAddress() public view returns(uint) {
+        return myAddress.balance;
+    }
 }
